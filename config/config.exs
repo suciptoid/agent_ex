ca045fbba8ad7a58100a4c67e87a0fac866136a6
Sucipto <sucipto@pm.me>
2026-03-08 10:40:25 +0700

    auth

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :app, :scopes,
+  user: [
+    default: true,
+    module: App.Users.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: App.UsersFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :app,
   ecto_repos: [App.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
